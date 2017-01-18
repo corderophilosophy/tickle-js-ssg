40d2bf71e8515d16866ab1a6f77d562d81bf5c6c
Dr. Rich Cordero <rich@corderophilosophy.com>
2017-01-18 08:38:49 -0500

    Add `date` to post meta-date-uh 😆

--- src/readFiles.js
+++ src/readFiles.js
@@ -8,11 +8,13 @@ module.exports = function readFiles(path) {
   fs.ensureDirSync(path);
   fs.readdirSync(path).forEach(file => {
     let date = file.match(/^(\d{4})-(\d\d?)-(\d\d?)-(.+)\.(md|markdown|html)$/);
-    if (!date)
+    if (!date) {
       return;
+    }
+    const d = `${date[3]}.${date[2]}.${date[1]}`
     const post = parser(path + '/' + file);
     const name = date[4];
-    const Post = Object.assign(post, { main: marked(post.main), name: name });
+    const Post = Object.assign(post, { main: marked(post.main), name: name, date: d });
     posts.push(Post);
   });
   return posts;
